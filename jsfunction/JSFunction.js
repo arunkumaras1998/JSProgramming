d1b89ed55fb1f7c4965dcc8f0047042e8b08d22b
Arun Kumar A S <arun1998.ag12@gmail.com>
2026-05-13 18:53:46 +0530

    JS Function Schema

---
+++ jsfunction/JSFunction.js
@@ -0,0 +1,24 @@
+// function without param
+
+// function with param
+
+// pass by value
+
+// pas by reference
+
+// default params
+
+// we can override the default value
+
+// function with return statement
+
+
+// call back function
+
+// calucalator
+// inside call add sub
+
+
+/// arrow function
+
+/// console.log(inside arrow function)
